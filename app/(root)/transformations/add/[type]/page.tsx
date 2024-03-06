148449d5195a8f7bcb06bbea473a6710f5bff23f
APayes <apayes@cdvi.ca>
2024-03-06 14:10:27 -0500

    Add Image Form...

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,22 @@
 // http://localhost:3000/transformations/add/rock
 
-const AddTransformationTypePage = () => {
-  return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
 
-export default AddTransformationTypePage
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+
+  return (
+    <>
+      <Header 
+        title={transformation.title} 
+        subtitle={transformation.subTitle} 
+      />
+
+      <TransformationForm />
+    </>
+  );
+};
+
+export default AddTransformationTypePage;
