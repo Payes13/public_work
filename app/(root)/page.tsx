4552366586c05d1bd550c5217dfc1df8167a9b11
Alexander Payes <alexander.payes.13@gmail.com>
2024-06-15 17:44:31 -0400

    globals.css, app/root paget.tsx, Collection.tsx: paragraph header added, menu in header removed, Recent Image Transformation added, hidden removed from css file

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -15,8 +15,13 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash Your Creative Vision With lostcanvas
+          Unleash Your Creative Vision With lostcanvas and AI
         </h1>
+
+        <p className="p-22-medium text-center text-white w-11/12">
+          Welcome to our cutting-edge SaaS application, where state-of-the-art AI transforms your images with ease. Effortlessly remove unwanted objects and backgrounds, recolor specific elements, and intelligently fill gaps for a flawless finish. Enhance image quality and detail with our refine feature. Experience the future of image editing with our comprehensive AI-powered tools, designed for all your creative needs.
+        </p>
+{/* 
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
             <Link
@@ -25,12 +30,12 @@ const Home = async ({ searchParams }: SearchParamProps) => {
               className="flex-center flex-col gap-2"
             >
               <li className="flex-center w-fit rounded-full bg-white p-4">
-                <Image src={link.icon} alt="image" width={24} height={24} />
+                <Image src={link.icon} alt="image" width={18} height={18} />
               </li>
               <p className="p-14-medium text-center text-white">{link.label}</p>
             </Link>
           ))}
-        </ul>
+        </ul> */}
       </section>
 
       <section className="sm:mt-12">
